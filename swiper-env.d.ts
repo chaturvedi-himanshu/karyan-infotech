9b07300b1b6713a3481116d7e821e0e21d52b423
himanshu chaturvedi <himanshu.chaturvedi@ourshopee.com>
2026-05-05 20:54:36 +0530

    done

--- swiper-env.d.ts
+++ swiper-env.d.ts
@@ -4,3 +4,11 @@ declare module "swiper/bundle" {
   const Swiper: any;
   export default Swiper;
 }
+
+declare module "swiper" {
+  // Swiper 8 module exports (Autoplay, Navigation, etc) can fail on strict export/type resolution in CI.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export const Autoplay: any;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  export const Navigation: any;
+}
